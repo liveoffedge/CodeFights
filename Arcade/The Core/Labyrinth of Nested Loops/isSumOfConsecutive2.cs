424ba8f80688688c61e24b1528d08580b2dac156
Vivek Prakash Sharma <viveksharma2087@gmail.com>
2018-05-07 19:43:31 +0530

    Create isSumOfConsecutive2.cs

---
+++ Arcade/The Core/Labyrinth of Nested Loops/isSumOfConsecutive2.cs
@@ -0,0 +1,15 @@
+int isSumOfConsecutive2(int n) {
+int result = 0;
+    for (int start = 1; start < n; start++) {
+        int number = n,
+        subtrahend = start;
+        while (number > 0) {
+            number -= subtrahend;
+            subtrahend++;
+        }
+        if (number==0) {
+            result++;
+        }
+    }
+      return result;
+}
