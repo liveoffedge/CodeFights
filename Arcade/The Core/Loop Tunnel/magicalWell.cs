adfb9b3e82376047285df15dc0e53d11a9ca8d1b
liveoffedge <viveksharma2087@gmail.com>
2018-05-07 17:45:43 +0530

    Create magicalWell.cs

---
+++ Arcade/The Core/Loop Tunnel/magicalWell.cs
@@ -0,0 +1,10 @@
+int magicalWell(int a, int b, int n) {
+    int result = 0;
+    for(int i = 0;i<n;i++)
+    {
+        result += (a*b);
+        a++;
+        b++;        
+    }
+    return result;
+}
