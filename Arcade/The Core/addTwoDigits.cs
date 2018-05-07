e5530264c7e35dc88ca49c06f20f647d1f118b91
liveoffedge <viveksharma2087@gmail.com>
2018-05-07 17:22:27 +0530

    Create addTwoDigits.cs

---
+++ Arcade/The Core/addTwoDigits.cs
@@ -0,0 +1,12 @@
+int addTwoDigits(int n) {
+    int result = 0;
+    if(n >= 10 && n <= 99)
+    {
+      result = n%10;
+      if(n/10 > 0)
+      {
+        result += n/10;
+      }
+    }
+    return result;
+}
