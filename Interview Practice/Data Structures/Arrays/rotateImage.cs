c880ba119c3fc839612d502003c98d09d6612b9e
Vivek Prakash Sharma <viveksharma2087@gmail.com>
2018-05-07 21:02:46 +0530

    Create rotateImage.cs

---
+++ Interview Practice/Data Structures/Arrays/rotateImage.cs
@@ -0,0 +1,26 @@
+int[][] rotateImage(int[][] a) {
+int temp = 0;
+            int n = a.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < a[0].Length; j++)
+                {
+                    temp = 0;
+                    temp = a[i][j];
+                    a[i][j] = a[j][i];
+                    a[j][i] = temp;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
+                {
+                    temp = 0;
+                    temp = a[i][j];
+                    a[i][j] = a[i][n - 1 - j];
+                    a[i][n - 1 - j] = temp;
+                }
+            }
+
+            return a;
+}
