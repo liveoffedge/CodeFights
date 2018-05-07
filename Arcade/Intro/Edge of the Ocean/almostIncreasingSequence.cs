64ea65936cd08632d016ef51c275441525b5fc4e
Vivek Prakash Sharma <viveksharma2087@gmail.com>
2018-05-07 20:55:08 +0530

    Create almostIncreasingSequence.cs

---
+++ Arcade/Intro/Edge of the Ocean/almostIncreasingSequence.cs
@@ -0,0 +1,22 @@
+bool almostIncreasingSequence(int[] sequence) {    
+    var found = false;
+  for (var i=1;i<sequence.Length;i++) {
+    if(sequence[i] <= sequence[i-1]) {
+      if(found) {
+        return false;
+      }
+      found = true;
+       
+      if(i == 1 || i + 1 == sequence.Length) {
+        continue;
+      }
+      else if (sequence[i] > sequence[i-2]) {
+        sequence[i-1] = sequence[i-2];
+      }
+      else if(sequence[i-1] >= sequence[i+1]) {
+        return false;
+      }
+    }
+  }
+  return true;        
+}
