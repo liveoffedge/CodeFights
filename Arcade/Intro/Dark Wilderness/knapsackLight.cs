b7107f66bddeb628885ac58931753c5b298b4143
Vivek Prakash Sharma <viveksharma2087@gmail.com>
2018-05-07 20:03:52 +0530

    Create knapsackLight.cs

---
+++ Arcade/Intro/Dark Wilderness/knapsackLight.cs
@@ -0,0 +1,12 @@
+int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+if (weight1 + weight2 <= maxW)
+        return value1 + value2;
+    else if (weight1 > maxW && weight2 > maxW)
+        return 0;
+    else if (weight1 > maxW)
+        return value2;
+    else if (weight2 > maxW)
+        return value1;
+    else
+        return value1 > value2 ? value1 : value2;
+}
