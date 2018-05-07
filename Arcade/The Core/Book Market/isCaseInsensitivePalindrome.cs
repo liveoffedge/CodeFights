1dbc40451320c8f061ac99a1bcd891a475106af6
Vivek Prakash Sharma <viveksharma2087@gmail.com>
2018-05-07 19:40:12 +0530

    Create isCaseInsensitivePalindrome.cs

---
+++ Arcade/The Core/Book Market/isCaseInsensitivePalindrome.cs
@@ -0,0 +1,20 @@
+bool isCaseInsensitivePalindrome(string inputString) {
+string value = inputString;
+    int min = 0;
+        int max = value.Length - 1;
+        while (true)
+        {
+            if (min > max)
+            {
+                return true;
+            }
+            char a = value[min];
+            char b = value[max];
+            if (char.ToLower(a) != char.ToLower(b))
+            {
+                return false;
+            }
+            min++;
+            max--;
+        }
+}
