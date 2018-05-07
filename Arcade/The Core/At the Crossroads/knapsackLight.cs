f90088a33d0d1368c0b26d8016745d8bc4262dbb
liveoffedge <viveksharma2087@gmail.com>
2018-05-07 17:33:49 +0530

    Create knapsackLight.cs

---
+++ Arcade/The Core/At the Crossroads/knapsackLight.cs
@@ -0,0 +1,32 @@
+int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+    if(weight1+weight2 <= maxW)
+    {
+        return value1+value2;
+    }
+    else
+    {
+        if(value1>value2)
+        {
+            if(weight1 <= maxW)
+            {
+                return value1;
+            }
+            else if(weight2 <= maxW)
+            {
+                return value2;
+            }
+        }
+        else
+        {
+            if(weight2 <= maxW)
+            {  
+                return value2;
+            }
+            else if(weight1 <= maxW)
+            {
+                return value1;
+            }
+        }
+    }
+    return 0;
+}
